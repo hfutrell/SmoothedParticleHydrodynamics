6f3db5ad48df76353dde1ceab1820d2afa48900d
Holmes Futrell <holmes@gingerlabs.com>
2016-08-25 15:36:19 -0700

    empty XCode project.

---
+++ SmoothedParticleHydrodynamics/SmoothedParticleHydrodynamics/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SmoothedParticleHydrodynamics
+//
+//  Created by Holmes Futrell on 8/25/16.
+//  Copyright © 2016 Holmes Futrell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
