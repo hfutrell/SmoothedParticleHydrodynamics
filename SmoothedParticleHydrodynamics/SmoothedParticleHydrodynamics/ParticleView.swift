d98e96874c5eb370c4dfd6cc5e95ecd2d5311eef
Holmes Futrell <holmes@gingerlabs.com>
2016-08-25 16:52:40 -0700

    custom view added.

---
+++ SmoothedParticleHydrodynamics/SmoothedParticleHydrodynamics/ParticleView.swift
@@ -0,0 +1,17 @@
+//
+//  ParticleView.swift
+//  SmoothedParticleHydrodynamics
+//
+//  Created by Holmes Futrell on 8/25/16.
+//  Copyright © 2016 Holmes Futrell. All rights reserved.
+//
+
+import Cocoa
+
+class ParticleView : NSView {
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        NSLog("particle view init")
+    }
+    
+}
